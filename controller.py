88ee1be39b5222ede55d6cabaa804fd98f61ce71
dejanualex <dejanualexandru@gmail.com>
2024-04-16 15:18:51 +0300

    nice output
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- controller.py
+++ controller.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import munch
 from pykube import KubeConfig, HTTPClient, object_factory, all, Pod
 
 # load Kubernetes configuration from the default location
@@ -9,16 +8,16 @@ api = HTTPClient(KubeConfig.from_env())
 ChaosAgent = object_factory(api, "blackadder.io/v1alpha1", "ChaosAgent")
 
 # retrieve ca config from pykube.query.Query objects
-agents = ChaosAgent.objects(api, namespace=all)
-for a in agents:
-    print ("ChaosAgent object named {} with specs:{}".format(a, a.obj["spec"]))
+# agents = ChaosAgent.objects(api, namespace=all)
+# for a in agents:
+#     print ("ChaosAgent object named {} with specs:{}".format(a, a.obj["spec"]))
 
 
 def main():
     """ operator aka custom loop """
     while True:
         for pod in Pod.objects(api).filter(namespace=all,selector={'target': 'yes'}):
-            print(pod.namespace, pod.name)
+            print("Found the little fucker {1} in namespace {0}".format(pod.namespace, pod.name)
 
 
 if __name__ == "__main__":
